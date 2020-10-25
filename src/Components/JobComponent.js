128daa17075d9df2a38bcd1966a692bc68b8a7cb
bhawna chopra <55947219+bhawnachopra2002@users.noreply.github.com>
2020-10-25 18:16:07 +0530

    Jobs updated

--- src/Components/JobComponent.js
+++ src/Components/JobComponent.js
@@ -3,13 +3,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Card, CardSubtitle, CardText, CardTitle,CardBody,Button } from 'reactstrap';
 
 const  RenderJob= (props) => {
+    console.log(props.timing)
     return(
-        <Card key={props.id} className="job">
+        <Card key={props.id} className="job" >
             <CardBody>
                 <CardTitle style={{fontWeight:500}}>{props.title}</CardTitle>
                 <CardSubtitle >{props.subtitle}</CardSubtitle>
                 <CardText>{props.desc}</CardText>
-                <Button className="details">Details</Button>
+                {props.timing && <CardText>Full Time Job.</CardText>}
+                {!props.timing && <CardText>Part Time Job.</CardText>}
+                <Button className="details" >Details</Button>
             </CardBody>
         </Card>
     )
--- src/pages/JobPage.js
+++ src/pages/JobPage.js
@@ -4,6 +4,7 @@ import {withRouter} from 'react-router-dom';
 import RenderJob from '../Components/JobComponent';
 import { connect } from 'react-redux';
 import { requestJobs } from '../actions/asyncActions';
+import { CardDeck } from 'reactstrap';
 
 class Job extends Component{
 
@@ -14,30 +15,107 @@ class Job extends Component{
     }
 
     render(){
-        const job1={
-            id:1,
-            title:'Software Engineer',
-            subtitle:'C++,Python',
-            desc:'lorem ipsum text big enought to cover about three lines'
-        }
+        console.log(this.props.jobs)
         const jobs = this.props.jobs;
+        const jobs2=[
+            {
+                address: "201 Anmol Sagar Darshan, 9/1 New palasia, near mangilal Dhoodh wala, Indore",
+                desc: "To Handle GST Working at the office.",
+                employer: "5f5b8bce2d23f7001746d920",
+                employerName: "raja_2208",
+                fromHome: false,
+                fullTime: true,
+                permanent: true,
+                reason: "Get full exposure of Independent working in GST Regime↵↵↵↵↵↵↵↵↵↵↵↵↵↵",
+                reqs: "At least 1-2 year Experience of practical working. Proficient in GST compliance is required. Person having post Articleship Experience in any Good CA office will be given preference.",
+                salary: "20000 to 25000",
+                title: "Chartered Accountant ",
+                __v: 0,
+                _id: "5f638af849f8690017676ce1"
+            },
+            {
+                address: "201 Anmol Sagar Darshan, 9/1 New palasia, near mangilal Dhoodh wala, Indore",
+                desc: "This is a software development Job wherein you need to be bla bla bla bla",
+                employer: "5f5b8bce2d23f7001746d920",
+                employerName: "Google",
+                fromHome: false,
+                fullTime: false,
+                permanent: true,
+                reason: "Get full exposure of Independent working in GST Regime↵↵↵↵↵↵↵↵↵↵↵↵↵↵",
+                reqs: "At least 1-2 year Experience of practical working. Proficient in GST compliance is required. Person having post Articleship Experience in any Good CA office will be given preference.",
+                salary: "20000 to 25000",
+                title: "Software Engineer",
+                __v: 0,
+                _id: "5f638af849f8690017576ce1"
+            },
+            {
+                address: "201 Anmol Sagar Darshan, 9/1 New palasia, near mangilal Dhoodh wala, Indore",
+                desc: "This is a teaching job wherein you have to give a lot of assignments all the time",
+                employer: "5f5b8bce2d23f7001746d920",
+                employerName: "IIT Jodhpur",
+                fromHome: false,
+                fullTime: false,
+                permanent: true,
+                reason: "Get full exposure of Independent working in GST Regime↵↵↵↵↵↵↵↵↵↵↵↵↵↵",
+                reqs: "At least 1-2 year Experience of practical working. Proficient in GST compliance is required. Person having post Articleship Experience in any Good CA office will be given preference.",
+                salary: "20000 to 25000",
+                title: "Software Engineer",
+                __v: 0,
+                _id: "5f938af849f8690017576ce1"
+            },
+            {
+                address: "201 Anmol Sagar Darshan, 9/1 New palasia, near mangilal Dhoodh wala, Indore",
+                desc: "This is a management Job wherein you need to be",
+                employer: "5f5b8bce2d23f7001746d920",
+                employerName: "PCTE group of colleges",
+                fromHome: false,
+                fullTime: false,
+                permanent: true,
+                reason: "Get full exposure of Independent working in GST Regime↵↵↵↵↵↵↵↵↵↵↵↵↵↵",
+                reqs: "At least 1-2 year Experience of practical working. Proficient in GST compliance is required. Person having post Articleship Experience in any Good CA office will be given preference.",
+                salary: "20000 to 25000",
+                title: "Managing Director",
+                __v: 0,
+                _id: "5f638bf849f8690017576ce1"
+            },
+            {
+                address: "201 Anmol Sagar Darshan, 9/1 New palasia, near mangilal Dhoodh wala, Indore",
+                desc: "This is a hardware development Job wherein you need to be bla bla bla bla xyz xyz xyz xy z xy z",
+                employer: "5f5b8bce2d23f7001746d920",
+                employerName: "Facebook",
+                fromHome: false,
+                fullTime: false,
+                permanent: true,
+                reason: "Get full exposure of Independent working in GST Regime↵↵↵↵↵↵↵↵↵↵↵↵↵↵",
+                reqs: "At least 1-2 year Experience of practical working. Proficient in GST compliance is required. Person having post Articleship Experience in any Good CA office will be given preference.",
+                salary: "20000 to 25000",
+                title: "Hardware Engineer",
+                __v: 0,
+                _id: "5f638af849f8699017576ce1"
+            }
+            
+        ]
+        const Job=jobs2.map((job)=>{
+            return(
+                <div key={job._id} className="col-12 col-md-4 mt-5 d-flex">
+                        <RenderJob id={job._id} title={job.title} subtitle={job.employerName} desc={job.desc} timing={job.fullTime} />
+                </div>
+            )
+        })
         return(
             <div className="container">
                 <div className='col-12'>
                     <h3 className="text-center">Jobs</h3>
                     <hr />
                 </div>
+                
                 <div className="row">
-                    <div key={job1.id} className="col-12 col-md-4 mt-5">
-                        <RenderJob job={this.props.jobs} id={job1.id} title={job1.title} subtitle={job1.subtitle} desc={job1.desc}/>
-                    </div>
-                    <div key={job1.id} className="col-12 col-md-4 mt-5">
-                        <RenderJob id={job1.id} title={job1.title} subtitle={job1.subtitle} desc={job1.desc}/>
-                    </div>
-                    <div key={job1.id} className="col-12 col-md-4 mt-5">
-                        <RenderJob id={job1.id} title={job1.title} subtitle={job1.subtitle} desc={job1.desc}/>
-                    </div>
-                    
+                    <CardDeck>{Job}</CardDeck>
+                   {/* {Job} */}
+                   {/* <div key="1" className="col-12 col-md-4 mt-5">
+                        <RenderJob id='1234' title='Software engineer' subtitle='Google' 
+                        desc='This is a software development Job wherein you need to be bla bla bla bla' timing={true}/>
+                    </div> */}
                 </div>
             </div>
         )
