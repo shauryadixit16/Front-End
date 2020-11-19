7b7ebd2a7278e938cff3a2f12337164cf9b622e6
shauryadixit16 <shauryadixit1601@gmail.com>
2020-11-19 19:52:53 +0530

    Job component

--- src/Components/JobComponent.js
+++ src/Components/JobComponent.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card, CardSubtitle, CardText, CardTitle,CardBody,Button } from 'reactstrap';
-
+import { Card, CardSubtitle, CardText, CardTitle,CardBody,Button, Modal, ModalHeader, ModalBody,FormGroup, Label, Input } from 'reactstrap';
 const  RenderJob= (props) => {
     console.log(props.timing)
+    const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
     return(
+        <div>
         <Card key={props.id} className="job" >
             <CardBody>
                 <CardTitle style={{fontWeight:500}}>{props.title}</CardTitle>
@@ -12,10 +15,32 @@ const  RenderJob= (props) => {
                 <CardText>{props.desc}</CardText>
                 {props.timing && <CardText>Full Time Job.</CardText>}
                 {!props.timing && <CardText>Part Time Job.</CardText>}
-                <Button className="details" >Details</Button>
+                <Button className="details" onClick={() => toggle()}>Details</Button>
             </CardBody>
         </Card>
+        <Modal isOpen={modal} toggle={toggle}>
+          <ModalHeader toggle={toggle}>Details</ModalHeader>
+          <ModalBody>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <p style={{fontWeight:'bold'}}>Description</p>
+               <p>To handle GST working at the office.</p>
+               <p style={{fontWeight:'bold'}}>Requirement</p>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quam eius cum nostrum hic.</p>
+               <p style={{fontWeight:'bold'}}>What's in it for you</p>
+               <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem necessitatibus saepe nulla!</p>
+               <p style={{fontWeight:'bold'}}>Salary:10000-25000rs</p>
+               <p>Lorem ipsum, dolor sit amet consectetur adipisicing..</p>
+               <p style={{fontWeight:'bold'}}>Workplace Address</p>
+               <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt?.</p>
+          </ModalBody>
+          <FormGroup>
+        <Label for="exampleFile">Enter a Proposal</Label>
+        <Input type="file" name="file" id="exampleFile"/>
+      </FormGroup>
+      <Button outline color="success"onClick={toggle}>Apply</Button>{' '}
+        </Modal>
+        </div>
     )
 };
 
-export default RenderJob;
+export default RenderJob;   
